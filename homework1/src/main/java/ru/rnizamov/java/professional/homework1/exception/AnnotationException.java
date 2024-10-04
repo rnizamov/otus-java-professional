54103087ab274f355de8ea63156fa16975231cf3
Rinat.Nizamov <_megion_@mail.ru>
2024-10-04 04:46:13 +0300

    add homework 1

---
+++ homework1/src/main/java/ru/rnizamov/java/professional/homework1/exception/AnnotationException.java
@@ -0,0 +1,7 @@
+package ru.rnizamov.java.professional.homework1.exception;
+
+public class AnnotationException extends RuntimeException {
+    public AnnotationException(String message) {
+        super(message);
+    }
+}
